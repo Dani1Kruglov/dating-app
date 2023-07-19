cd707e6a0cac95626d2b3040d9b98ed19d3213b0
Dani1Kruglov <danilakruglov25@gmail.com>
2023-07-19 15:00:39 +0200

    Исправлена формула подсчета рейтинга пользователя

--- dating-app/app/Http/Controllers/MyPageController.php
+++ dating-app/app/Http/Controllers/MyPageController.php
@@ -8,11 +8,10 @@ class MyPageController extends Controller
 {
     public function __invoke()
     {
-        $user=auth()->user();
-        if($user->dislikes === 0 ) {$medianValue = $user->likes;}
-        else{$medianValue = round(($user->likes+$user->dislikes)/2);}
+        $user = auth()->user();
+        $total = $user->likes+$user->dislikes;
         if ($user->likes === 0) {$rating = 0;}
-        else {$rating = round((10*$medianValue)/$user->likes, 1);}
+        else {$rating = round((10*$user->likes)/$total, 1);}
 
 
         if (isset(auth()->user()->email_verified_at)) {$verification = 'Пройдена';}
